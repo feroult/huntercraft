77c022810cbd233aac9faa08facc0948164d0ef2
Fernando Ultremare <feroult@gmail.com>
2021-01-02 13:05:10 -0300

    init

--- start.sh
+++ start.sh
@@ -1,3 +1,7 @@
 #!/bin/bash -x
 
-screen -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
+start() {
+    screen -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
+}
+
+(cd $MINECRAFT_HOME && start)
