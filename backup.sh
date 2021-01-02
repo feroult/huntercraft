a41a56f13216cd13845d6e07358924fd41793a21
Fernando Ultremare <feroult@gmail.com>
2021-01-02 16:44:19 -0300

    init

--- backup.sh
+++ backup.sh
@@ -1,14 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
 
 GSUTIL=/usr/bin/gsutil
 
 TIME=$(date "+%Y%m%d-%H%M%S")
+TEMP_DIR="/tmp/minecraft_backup"
+SCREEN_OUTPUT="$TEMP_DIR/minecraft-screen.txt"
+BACKUP_FILE="$TEMP_DIR/minecraft-backup.zip"
+
+mkdir -p $TEMP_DIR
 
 backup() {
-    zip -r /tmp/minecraft-backup.zip $MINECRAFT_HOME
+    zip -r $BACKUP_FILE $MINECRAFT_HOME
     gsutil -m cp -R $MINECRAFT_HOME gs://$MINECRAFT_BUCKET/minecraft-$TIME.zip
 }
 
-screen -r mcs -X stuff '/save-all\n/save-off\n'
+screen -r mcs -X stuff "save-all\nsave-off\nsay backup-$TIME\n"
+
+screen -r mcs -X hardcopy $SCREEN_OUTPUT
+while [ -z "$(grep backup-$TIME /tmp/minecraft-screen.txt)" ]; do
+    sleep 1
+    screen -r mcs -X hardcopy $SCREEN_OUTPUT
+done
+
 backup
-screen -r mcs -X stuff '/save-on\n'
+
+screen -r mcs -X stuff "save-on\n"
