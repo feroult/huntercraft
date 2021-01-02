2e35b73c4ed72ca5cb3ea0b46fbb4915b21a9d1e
Fernando Ultremare <feroult@gmail.com>
2021-01-02 17:23:12 -0300

    init

--- respawn.sh
+++ respawn.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 pidof java >/dev/null
 if [[ $? -ne 0 ]] ; then    
-    echo "Restarting Spigot: $(date)" 
+    echo "Restarting Spigot: $(date)"  > /tmp/respawn.log
     screen -wipe mcs || true
     $DIR/start.sh
 fi
