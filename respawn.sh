116d4cd79977bc016918a7027419475cafa4c75d
Fernando Ultremare <feroult@gmail.com>
2021-01-02 17:22:09 -0300

    init

--- respawn.sh
+++ respawn.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 pidof java >/dev/null
 if [[ $? -ne 0 ]] ; then    
-    echo "Restarting Spigot: $(date)" >> /tmp/respawn.log
+    echo "Restarting Spigot: $(date)" 
     screen -wipe mcs || true
-    cd $MINECRAFT_HOME >> /tmp/respawn.log
-    echo "here" >> /tmp/respawn.log
-    ./start.sh >> /tmp/respawn.log
+    $DIR/start.sh
 fi
