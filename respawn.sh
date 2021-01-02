fab28c49337a00e739186026bfed82cac829f09b
Fernando Ultremare <feroult@gmail.com>
2021-01-02 17:16:04 -0300

    init

--- respawn.sh
+++ respawn.sh
@@ -2,7 +2,7 @@
 
 pidof java >/dev/null
 if [[ $? -ne 0 ]] ; then    
-    echo "Restarting Spigot: $(date)" > /tmp/respawn.log
+    echo "Restarting Spigot: $(date) $MINECRAFT_HOME" > /tmp/respawn.log
     screen -wipe mcs
     cd $MINECRAFT_HOME
     ./start.sh
